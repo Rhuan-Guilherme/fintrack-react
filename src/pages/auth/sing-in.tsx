8237dff735935b453c61ab2492fe7c797affdb6b
Rhuan Guilherme <rhuan23032004@gmail.com>
2025-05-13 21:53:29 -0300

    🎨📱 feat: Inicia estilização da página de login com abordagem mobile-first

--- src/pages/auth/sing-in.tsx
+++ src/pages/auth/sing-in.tsx
@@ -1,3 +1,56 @@
+import googleIcon from "../../assets/icon-google.svg";
+
 export function SingIn() {
-  return <h1>SingIn</h1>;
+  return (
+    <>
+      <div>
+        <h1 className="font-poppins text-3xl font-semibold text-gray-700">
+          Acesse sua conta
+        </h1>
+        <p className="font-roboto text-xs font-semibold text-gray-500">
+          O seu app para controle financeiro.
+        </p>
+      </div>
+
+      <div className="font-roboto mt-5 space-y-5 font-semibold text-gray-500">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            className="rounded-md border-1 border-gray-300 p-2"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="password">Senha</label>
+          <input
+            type="password"
+            id="password"
+            className="rounded-md border-1 border-gray-300 p-2"
+          />
+        </div>
+        <button className="w-full cursor-pointer rounded-md bg-indigo-600 px-4 py-2 text-gray-100">
+          Entrar
+        </button>
+      </div>
+
+      <div className="font-roboto my-4 flex items-center">
+        <div className="flex-grow border-t border-gray-300"></div>
+        <span className="mx-4 text-sm text-gray-500">ou</span>
+        <div className="flex-grow border-t border-gray-300"></div>
+      </div>
+
+      <button className="flex w-full cursor-pointer items-center justify-center gap-4 rounded-md border-1 border-gray-300 px-4 py-2 text-gray-500">
+        <img src={googleIcon} />
+        Entrar com o Google
+      </button>
+
+      <div className="font-roboto mt-5 flex items-center justify-center rounded-md bg-gray-100 p-2 text-[14px] text-gray-500">
+        Novo no Fintrack?
+        <span className="ml-1.5 font-semibold text-indigo-600">
+          Crie uma conta
+        </span>
+      </div>
+    </>
+  );
 }
--- src/pages/layouts/authLayout.tsx
+++ src/pages/layouts/authLayout.tsx
@@ -1,10 +1,13 @@
-import { Outlet } from 'react-router';
+import { Outlet } from "react-router";
 
 export function AuthLayout() {
   return (
-    <div>
-      <div>AuthLayout</div>
-      <div>
+    <div className="relative flex h-screen w-screen items-center justify-center bg-gray-200">
+      <div className="hidden">AuthLayout</div>
+      <h1 className="font-poppins font-xl absolute top-10 left-8 font-semibold">
+        Fintrack
+      </h1>
+      <div className="rounded-md bg-gray-50 p-5 shadow">
         <Outlet />
       </div>
     </div>
